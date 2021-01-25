13f7be0e9fd76339f35e661008763f1b30f736ac
potato <37996571+potato965@users.noreply.github.com>
2021-01-25 21:29:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,17 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+mkdir -p package/custom
+cd package/custom
+git clone https://github.com/rufengsuixing/luci-app-adguardhome
+git clone -b lede https://github.com/pymumu/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+#git clone https://github.com/Lienol/openwrt-package
+git clone https://github.com/jerrykuku/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr
+#git clone https://github.com/jerrykuku/openwrt-package
+#git clone https://github.com/fw876/helloworld
+cd ../../
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
